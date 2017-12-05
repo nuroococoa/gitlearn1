ac841abca66801d7a0bf63dcf838033a197a4f7b
nuroo cocoa <acatundercar@gmail.com>
2017-12-06 08:00:01 +0900

    Add connection to database

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 var cool = require('cool-ascii-faces');
 var express = require('express');
+var mysql = require('mysql');
+var client = mysql.createConnection({
+  host : 'us-cdbr-iron-east-05.cleardb.net',
+  user :'bb85c2786f4782',
+  password : '56365829'
+});
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
@@ -13,6 +19,19 @@ app.set('view engine', 'ejs');
 app.get('/', function(request, response) {
   response.render('pages/index')
 });
+
+app.get('/physicist', (req, res) => {
+  client.query('use heroku_ff5ef326239cabc');
+  client.query('select * from physicist', (err, result, fields) =>{
+    if (err) {
+      console.log("there's an error in query context")
+    } else {
+      console.log(result);
+    }
+  });
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end('<h1>express basic</h1>');
+})
 
 app.get('/cool', function(request, response) {
   response.send(cool());
